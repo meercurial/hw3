e268dd327a7154ff12e16cf934f77efebe1acbf0
Alexander <iwankowi495@gmail.com>
2024-08-17 12:16:25 +0300

    three commit

--- src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import {pureAddUserCallback} from '../HW3'
+import React from "react";
+import { pureAddUserCallback, UserType } from "../HW3";
 
-let initialState: any[]
-const setName = (a: any[]) => {
-    initialState = a
-}
+let initialState: UserType[] = [];
+const setUsers = (a: React.SetStateAction<UserType[]>) => {
+  initialState = Array.isArray(a)
+    ? a
+    : [...(initialState || []), ...(Array.isArray(a) ? a : [a])];
+};
 
 beforeEach(() => {
-    initialState = []
-})
+  initialState = [];
+});
 
-test('name 1', () => {
-    pureAddUserCallback('name', setName, initialState)
-    expect(initialState.length).toBe(1)
-    expect(initialState[0].name).toBe('name')
-    expect(!!initialState[0]._id).toBe(true)
-})
+test("name 1", () => {
+  pureAddUserCallback("name", setUsers, initialState);
+  expect(initialState.length).toBe(1);
+  expect(initialState[0].name).toBe("name");
+  expect(!!initialState[0]._id).toBe(true);
+});
